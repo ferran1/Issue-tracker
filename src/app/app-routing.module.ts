39376bd7ef4d7fbd757b4fac680ef491a19c5994
ferran tombal <ferran.tombal@hva.nl>
2020-02-07 23:16:23 +0100

    completed navbar and created routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {OverviewComponent} from './components/overview/overview.component';
 
-
-const routes: Routes = [];
+/**
+ * Defines all the routes in the application
+ */
+const routes: Routes = [
+  {path: '', redirectTo: 'overview', pathMatch: 'full'},
+  {
+    path: 'overview', component: OverviewComponent
+  },
+  {
+    path: '**', component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { OverviewComponent } from './components/overview/overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PageNotFoundComponent,
+    OverviewComponent,
   ],
   imports: [
     BrowserModule,
